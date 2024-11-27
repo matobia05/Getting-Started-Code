cadee2fb317900cfbdd9aea273087de2ffb4a1b4
Mena Tobia <matobia@unc.edu>
2024-11-27 08:45:50 -0500

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Mena Tobia on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
