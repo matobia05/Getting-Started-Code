ced6d9480927e82bbfaea4f201ff4accf5e5a0f3
Mena Tobia <156716207+matobia05@users.noreply.github.com>
2024-12-02 22:10:54 -0500

    Views, Structures, and properties

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Mena Tobia on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
