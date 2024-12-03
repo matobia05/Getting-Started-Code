4aa30a94388bbce269198d45767d0d92515005f8
Mena Tobia <156716207+matobia05@users.noreply.github.com>
2024-12-02 22:15:55 -0500

    Lists and Text Fields

---
+++ Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Mena Tobia on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
